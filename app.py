4367a0b45eb1074f032cfe5281e26eb472c9d760
priyabhartiimpinge <priyabharti.impinge@gmail.com>
2020-02-19 17:06:23 +0530

    sdfgkjidshyfuhds

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
